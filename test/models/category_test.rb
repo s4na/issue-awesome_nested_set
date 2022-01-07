087d104171907ed020b8e033e11f04e6618b5f2d
s4na <appletea.umauma@gmail.com>
2022-01-07 18:10:58 +0900

    Category に acts_as_nested_set を追加した上で、動作をテストする

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -2,7 +2,14 @@ require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
   test "the truth" do
-    category = Category.create(name: 'name1', lft: 1, rgt: 1, parent_id: nil)
-    assert category.save
+    # https://github.com/collectiveidea/awesome_nested_set/wiki/Awesome-nested-set-cheat-sheet
+    science = Category.create!(:name => 'Science')
+    assert science
+
+    physics = Category.create!(:name => 'Physics')
+    physics.move_to_child_of(science)
+    assert physics.parent
+
+    assert_equal physics.parent, science
   end
 end
