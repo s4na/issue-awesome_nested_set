2fb9edc16f52364fbbffef2442ba69560866df90
s4na <appletea.umauma@gmail.com>
2022-01-07 15:25:44 +0900

    コードフォーマット

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
   test "the truth" do
-    category = Category.create(name: 'name1',lft: 1, rgt: 1, parent_id: nil)
+    category = Category.create(name: 'name1', lft: 1, rgt: 1, parent_id: nil)
     assert category.save
   end
 end
