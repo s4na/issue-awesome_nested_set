1b4e8e622927f66059b383e4e8c0173f6a10053b
s4na <appletea.umauma@gmail.com>
2022-01-07 15:23:40 +0900

    Categroyのテスト追加

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    category = Category.create(name: 'name1',lft: 1, rgt: 1, parent_id: nil)
+    assert category.save
+  end
 end
