1fa9078b3b1dfd0e30bf3f2121f896e073b75184
s4na <appletea.umauma@gmail.com>
2022-01-11 14:29:32 +0900

    config.active_record.has_many_inversing = false

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module RailsAwesomeNestedSetSandbox
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.active_record.has_many_inversing = false
   end
 end
